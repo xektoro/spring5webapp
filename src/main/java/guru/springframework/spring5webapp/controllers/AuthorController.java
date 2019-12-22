f2687c130c21aa059e38b014cfaf3de1da8cad56
Dimitar Kostov <xektoro@abv.bg>
2019-12-22 19:28:31 +0200

    Added some doc files and png files

--- src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// when we annotate a class as Controller, so it is going to be Spring Component, which will be managed by Spring Context
+// So when Spring starts up, he will get a handle on this and will crate an instance of it and put that instance into Spring Context
 @Controller
 public class AuthorController {
     private AuthorRepository authorRepository;
